fec2c0696f63c3a132b86d07270e04d386691295
ClaireLanders <122327886@umail.ucc.ie>
2026-04-09 23:31:00 +0100

    Task Card Component Created with count state

---
+++ components/TaskCard.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Button, Text, View } from 'react-native';
+
+type TaskCardProps = {
+    name: string;
+    category: string;
+    date: string;
+};
+
+export default function TaskCard({ name, category, date }:
+TaskCardProps) {
+    const [count, setCount] = useState(0);
+
+    return (
+    <View style={{ marginBottom: 12, padding: 10, borderWidth: 1 }}>
+        <Text style={{ fontSize: 18 }}>{name}</Text>
+        <Text>Category: {category}</Text>
+        <Text>Date: {date}</Text>
+
+        <Text style={{marginTop: 10}}>Count: {count}</Text>
+        <Button title="+1" onPress={() => setCount(count + 1)} />
+        <Button title="-1" onPress={() => setCount(count - 1)} />
+
+        <Text>
+            {count > 0 && 'Positive'}
+            {count < 0 && 'Negative'}
+            {count === 0 && 'Zero' }
+        </Text>
+    </View>
+    );
+}

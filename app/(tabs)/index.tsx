ccdfd602106b71e0aa1a0b071533b08e0eaad05e
ClaireLanders <122327886@umail.ucc.ie>
2026-04-15 15:24:09 +0100

    adding state to the component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
 import TaskCard from '@/components/TaskCard';
+import { useState } from 'react';
 import { View } from 'react-native';
 
 export default function IndexScreen() {
+  const [tasks] = useState([
+    {id: 1, name:"Meal Prep", category:"Health", date:"2026-04-08"},
+    {id: 2, name:"Walk", category:"Health", date:"2026-04-09"},
+    {id: 3, name:"Plan essay", category:"College work", date:"2026-04-10"}
+  ]);
 return (
 <View style={{ padding: 20 }}>
-  <TaskCard name="Task 1" category="cat 1" date="2026-04-08"/>
-  <TaskCard name="Task2" category="cat 2" date="2026-04-09"/>
+  {tasks.map(task =>(
+    <TaskCard
+    key={task.id}
+    name={task.name}
+    category={task.category}
+    date={task.date}
+    /> 
+  ))}
 </View>
-  );
+);
 }

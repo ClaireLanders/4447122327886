14438efb595133fd61170c8d57ee53ba4d076345
ClaireLanders <122327886@umail.ucc.ie>
2026-04-15 20:40:36 +0100

    moving Habit data into parent , array state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,22 +2,24 @@ import HabitCard from '@/components/HabitCard';
 import { useState } from 'react';
 import { View } from 'react-native';
 
+
 export default function IndexScreen() {
   const [habits] = useState([
-    {id: 1, name:"Meal Prep", category:"Health", date:"2026-04-08"},
-    {id: 2, name:"Walk", category:"Health", date:"2026-04-09"},
-    {id: 3, name:"Plan essay", category:"College work", date:"2026-04-10"}
+    { id: 1, name: 'Morning Run', category: 'Fitness', date: '2026-04-08', count: 0 },
+    { id: 2, name: 'Meditation', category: 'Mindfulness', date: '2026-04-09', count: 0 },
+    { id: 3, name: 'Read a Book', category: 'Learning', date: '2026-04-10', count: 0 },
   ]);
+
 return (
-<View style={{ padding: 20 }}>
-  {habits.map(habit =>(
-    <HabitCard
-    key={habit.id}
-    name={habit.name}
-    category={habit.category}
-    date={habit.date}
-    /> 
-  ))}
-</View>
+  <View style={{ padding: 20 }}>
+    {habits.map(habit =>(
+      <HabitCard
+      key={habit.id}
+      name={habit.name}
+      category={habit.category}
+      date={habit.date}
+      /> 
+    ))}
+  </View>
 );
 }
